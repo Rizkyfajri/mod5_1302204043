080699924d4f4ca88cd9e12d93fd479a8f66e885
Rizky Fajri <mansamussolini@gmail.com>
2022-03-26 16:22:24 +0700

    Update SayaTubeUser.cs

--- mod5_1302204043/SayaTubeUser.cs
+++ mod5_1302204043/SayaTubeUser.cs
@@ -12,6 +12,38 @@ namespace mod5_1302204043
         private int ID;
         List<SayaTubeVideo> UploadedVideos;
 
+        public SayaTubeUser(string username)
+        {
+            try
+            {
+                if (this.username != null && username.Length < 200)//contract
+                {
+                    this.username = username;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("error : cannot be null or exceed 200 character limit");
+            }
+        }
 
+        public void AddVideo()
+        {
+
+        }
+        public void GetTotalVideoPlaycount()
+        {
+
+        }
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine("User : "+ username);
+            foreach (var data in UploadedVideos)
+            {
+                int vid = 1;
+                Console.WriteLine("Video "+vid+ " : "+UploadedVideos);
+                vid = vid + 1;
+            }
+        }
     }
 }
