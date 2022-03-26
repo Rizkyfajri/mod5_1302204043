7943808471ae74249283ce0db9611e7b70af37bc
Rizky Fajri <mansamussolini@gmail.com>
2022-03-26 15:40:00 +0700

    Initial commit

---
+++ mod5_1302204043/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace mod5_1302204043
+{
+    static class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
